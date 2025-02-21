fea9529bd764639921c8e2cbab3853d7228a8ca0
kanishkadeo27 <kanishkadeo92022@gmail.com>
2025-02-21 23:39:13 +0530

    profile image update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Profile from "../../assets/home1.jpg";
+import Profile from "../../assets/home_2.jpg";
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
 import "./home.css";
@@ -16,7 +16,14 @@ const Home = () => {
             Frontend Developer
           </h1>
           <p className="home__description">
-          I am a hard-working and ambitious individual with a strong career-oriented mindset, always eager to learn and embrace innovative ideas to achieve positive outcomes. With a proven ability to transform client concepts into aesthetically pleasing and functional designs, I thrive on challenges and strive to deliver exceptional results. My dedication to continuous learning and improvement fuels my passion for turning ideas into impactful solutions.
+            I am a hard-working and ambitious individual with a strong
+            career-oriented mindset, always eager to learn and embrace
+            innovative ideas to achieve positive outcomes. With a proven ability
+            to transform client concepts into aesthetically pleasing and
+            functional designs, I thrive on challenges and strive to deliver
+            exceptional results. My dedication to continuous learning and
+            improvement fuels my passion for turning ideas into impactful
+            solutions.
           </p>
           <Link to="/about" className="button">
             More About Me{" "}
